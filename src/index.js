1a8ea7e833afe6ee26d02061baac3cdb2f6b1c62
Ahmad Mostafa <33914189+ahmadmostafa10495@users.noreply.github.com>
2020-04-05 00:47:15 +0200

    trying github from bash with ssh

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
+// import Hello from './Hello';
+// import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,7 +13,18 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Hello greeting={'Hello React Ninja'}/>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+// ReactDOM.render(
+//   <React.StrictMode>
+//   	<CardList robots={ robots }/>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
