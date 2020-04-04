b81ffd39cafdc21582956195325c9612f016ef6f
Ahmad Mostafa <33914189+ahmadmostafa10495@users.noreply.github.com>
2020-04-05 00:58:08 +0200

    Updates

---
+++ precache-manifest.9c615fff4f591d43531047b332dd41d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ef608723e62f4d13cc32d15d24f2ae",
+    "url": "//ahmadmostafa10495.github.io/robo-friends/index.html"
+  },
+  {
+    "revision": "bd66817ae05f234735d8",
+    "url": "//ahmadmostafa10495.github.io/robo-friends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "068db466203df0530993",
+    "url": "//ahmadmostafa10495.github.io/robo-friends/static/css/main.ef42b2e7.chunk.css"
+  },
+  {
+    "revision": "bd66817ae05f234735d8",
+    "url": "//ahmadmostafa10495.github.io/robo-friends/static/js/2.6d5955fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//ahmadmostafa10495.github.io/robo-friends/static/js/2.6d5955fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068db466203df0530993",
+    "url": "//ahmadmostafa10495.github.io/robo-friends/static/js/main.b4da38ac.chunk.js"
+  },
+  {
+    "revision": "69adc01fce3546f5700f",
+    "url": "//ahmadmostafa10495.github.io/robo-friends/static/js/runtime-main.7d79b9f9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "//ahmadmostafa10495.github.io/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
